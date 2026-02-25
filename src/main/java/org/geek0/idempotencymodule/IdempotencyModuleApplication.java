abed950290da43d2be63a11690229e15e3a18430
Uthman Bakar <uthmanb@outlook.com>
2026-02-25 20:00:17 +0100

    New spring boot api setup

---
+++ src/main/java/org/geek0/idempotencymodule/IdempotencyModuleApplication.java
@@ -0,0 +1,13 @@
+package org.geek0.idempotencymodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotencyModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdempotencyModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/geek0/idempotencymodule/IdempotencyModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.geek0.idempotencymodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotencyModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
